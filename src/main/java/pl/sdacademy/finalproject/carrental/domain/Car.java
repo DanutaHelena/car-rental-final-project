9906ff32982f63908a9b4164513be603c65d7e18
bartlomiej-rabinski <bartlomiej.rabinski@o2.pl>
2021-08-24 18:39:38 +0200

    add Status ,

--- src/main/java/pl/sdacademy/finalproject/carrental/domain/Car.java
+++ src/main/java/pl/sdacademy/finalproject/carrental/domain/Car.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 @Data
 @Entity
@@ -14,7 +17,9 @@ public class Car {
 
     @Id
     //@Column(name = "plate_number")
+    @NotBlank
     private String plateNumber;
+    @NotBlank
     private String brand;
     private String model;
    // @Column(name = "body_type" )
@@ -24,6 +29,8 @@ public class Car {
     private String colour;
     @Enumerated(EnumType.STRING)
     private CarRentStatus status;
+    @Min(0)
+    @NotNull
     private Integer mileage;
     private BigDecimal cost;
 
